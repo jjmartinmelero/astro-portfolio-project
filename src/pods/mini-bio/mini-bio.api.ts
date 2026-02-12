8a4d888264deb8b8751eacf27b6f48a923bbe2ca
Juan Jesús Martín Melero <40661353+jjmartinmelero@users.noreply.github.com>
2026-02-12 09:46:34 +0000

    fixed getMiniBio in mini bio api file

--- src/pods/mini-bio/mini-bio.api.ts
+++ src/pods/mini-bio/mini-bio.api.ts
@@ -1,9 +1,7 @@
-import client from "#lib/client.ts";
-import type { MiniBio } from "./mini-bio.model";
+import client from '#lib/client.ts';
+import type { MiniBio } from './mini-bio.model';
 
-export const getMiniBio = async () => {
-    await client.getContent<MiniBio>({
-        contentType: "MiniBio",
-    })
-}
-
+export const getMiniBio = async () =>
+  await client.getContent<MiniBio>({
+    contentType: 'MiniBio',
+  });
